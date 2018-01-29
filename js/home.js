daa6496def4de15c548aa9917a799d2fd419897c
rosColunga <rosicolunga@hotmail.com>
2018-01-29 12:38:30 -0600

    Añadiendo funcionalidad de filtro y modales con data

--- js/home.js
+++ js/home.js
@@ -1,44 +1,162 @@
+/* Al cargar la página iniciaremos... */
+
 $(document).ready(function() {
 
+  /* Se guardan los parámetros de data en array para iterar */
+  var laboratorian = [{
+      key: 'FRIEND2'
+    },
+    {
+      key: 'FRIEND3'
+    },
+    {
+      key: 'FRIEND4'
+    },
+    {
+      key: 'FRIEND5'
+    },
+  ];
+
+  /* Se indica que los elementos de laboratorian sean recorridos para encontrar el nombre */
+  $.each(laboratorian, function findName(i) {
+    var keys = Object.keys(data[laboratorian[i].key]);
+    laboratorian[i].number = keys.length;
+  });
+
+  /*Arreglo con nombre de amigas en tu red (keys de laboratorian)*/
+  function nameArray() {
+    var who = [];
+    $.each(laboratorian, function getKeys(i) {
+      who.push(laboratorian[i].key);
+    });
+    //console.log();
+    return who;
+  };
+  /*Guradamos la función*/
+  var keys = nameArray();
+
+  /*El buscador filtrará por el nombre*/
+  function findName() {
+    var names = [];
+    $.each(keys, function laboratorianName(i) {
+      var showing = data[keys[i]];
+      /*Le pedimos que muestre el resultado iterando en keys*/
+      $.each(showing, function find(j) {
+        names.push(showing[j].name);
+      });
+    });
+    return names;
+  }
+
+  /* Filtraremos qué se muestra*/
+  $.each(keys, function laboratorianName(i) {
+    var showing = data[keys[i]];
+    $.each(showing, function showPic(j) {
+      // Template de Modal, jalando la data y estilos css
+      var structure = '<div class = "profPic pic-style inline-block" data-name="' + showing[j].wichGeneration + '" data-adress="' + showing[j].adress + '"data-major="' + showing[j].major + '"data-mail="' + showing[j].mail + '"' + ' id="' + showing[j].name + '">' +
+        '<div class="relPosition" data-toggle="modal" data-target="#modal"> <img class="pic-style" src=" ' + showing[j].image + '"' + 'id="' + showing[j].name + '"' + '">' +
+        ' <div class="mouseOpacity visibility-hidden"><p class="text-center msOver-text" > ' + showing[j].name + '</p> </div></div></div>';
+      // Añadimos al final del div clase "photo-gallery", la estructura descrita arriba
+      $('.photo-gallery').append(structure);
+    });
+  });
+
+  /*Búsqueda con entrada de texto en input
+  con evento en teclado (keyboard -keyup-)*/
+  var inputSearch = $('.input-js');
+  $(inputSearch).on('keyup', function() {
+    var name = $(this).val().toLowerCase();
+    $('.profPic').hide();
+    $('.profPic').each(function() {
+      if ($(this).attr('id').toLowerCase().indexOf(name) !== -1) {
+        $(this).show();
+      }
+    });
+  });
+
+  /* Efecto Mouseover (relativo a contenedor) */
+  $('.relPosition').on('mouseover', function() {
+    /*Remover clase "esconder"*/
+    $(this).children().last().removeClass('visibility-hidden');
+  });
+  /* Segunda aplicación retira estilo al contacto del mouse... (out)*/
+  $('.relPosition').on('mouseout', function() {
+    $(this).children().last().addClass('visibility-hidden');
+  });
+
+  /* Salida de data en modales */
+  $('.relPosition').on('click', function() {
+    var title = $('.modal-name-title');
+    var wichGeneration = $('.place-name-data');
+    var adress = $('.adress-data');
+    var mail = $('.mail-data');
+    var major = $('.major-data');
+    var keys = nameArray();
+
+    /* Salida de data en texto dentro de los modales */
+    $(title).text($(this).children().first().attr('id')); /* showing[j].name */
+    $(wichGeneration).text($(this).parent().data('name'));
+    $(adress).text($(this).parent().data('adress'));
+    $(major).text($(this).parent().data('major'));
+    $(mail).text($(this).parent().data('mail'));
+  });
+
+
   /***Función para crear un nuevo post***/
-  var post = function(e){
-  e.preventDefault;
-  //guardando los values de los inputs
-   var $postTitle = $('#post-title').val().toUpperCase(        );
-   var $postBody = $('#post-body').val();
+  var post = function(e) {
+    e.preventDefault;
+    //guardando los values de los inputs
+    var $postTitle = $('#post-title').val().toUpperCase();
+    var $postBody = $('#post-body').val();
 
-   //creando los elementos
-   var $post = $('<article/>',{'class':'post'});
-   var $title = $('<h2/>',{'class':'title'});
-   //var $line = $('<hr/>',{'class':'line'});
-   var $row = $('<div/>',{'class':'row'});
-   var $body = $('<p/>', {'class':'body','class':'col-sm-10'});
-   var $btnCol = $('<div/>',{'class':'col-sm-2', 'class':'col-md-2'});
-   var $commentBtn = $('<button/>',{'class':'btn','class':'btn-primary','type':'submit'})
+    //creando los elementos
+    var $post = $('<article/>', {
+      'class': 'post'
+    });
+    var $title = $('<h2/>', {
+      'class': 'title'
+    });
+    //var $line = $('<hr/>',{'class':'line'});
+    var $row = $('<div/>', {
+      'class': 'row'
+    });
+    var $body = $('<p/>', {
+      'class': 'body',
+      'class': 'col-sm-10'
+    });
+    var $btnCol = $('<div/>', {
+      'class': 'col-sm-2',
+      'class': 'col-md-2'
+    });
+    var $commentBtn = $('<button/>', {
+      'class': 'btn',
+      'class': 'btn-primary',
+      'type': 'submit'
+    })
 
-  //dando atributos
-   $title.text($postTitle);
-   $body.text($postBody);
-   $commentBtn.text('More...')
+    //dando atributos
+    $title.text($postTitle);
+    $body.text($postBody);
+    $commentBtn.text('More...')
 
-  //Colocando en el HTML
-  $($btnCol).append($commentBtn);
-  $($row).append($body);
-  $($row).append($btnCol);
-  $($post).append($title);
-  $($post).append($row);
-  $('#container').prepend($post);
+    //Colocando en el HTML
+    $($btnCol).append($commentBtn);
+    $($row).append($body);
+    $($row).append($btnCol);
+    $($post).append($title);
+    $($post).append($row);
+    $('#container').prepend($post);
 
-  clear();
+    clear();
   }
   $('#post-btn').click(post);
 
 
   /***Función para limpiar los inputs***/
-  var clear = function(){
+  var clear = function() {
     //Limpiando los inputs
-     $('#post-title').val('');
-     $('#post-body').val('');
+    $('#post-title').val('');
+    $('#post-body').val('');
   }
 
 });
