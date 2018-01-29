54784d80f3f0d1d174cf9dc5c6ad1865615b9280
rosColunga <rosicolunga@hotmail.com>
2018-01-29 12:39:38 -0600

    Información de laboratorians

---
+++ js/data.js
@@ -0,0 +1,38 @@
+/* Nombramos variable "data" con la información de laboratorians*/
+var data = {
+  /*Ciudades=key, por orden alfabético */
+  'FRIEND2': [{
+    name: 'Alejandra López',
+    image: '../assets/images/friend2.jpg',
+    wichGeneration: '3° Generación <l>',
+    adress: 'Arequipa',
+    mail: "alelo@yahoo.com",
+    major: 'Front-end Developer',
+  }, ],
+  'FRIEND3': [{
+      name: 'María González',
+      image: '../assets/images/friend3.jpg',
+      wichGeneration: '2° Generación <l>',
+      adress: 'Perú',
+      mail: "mary_glez@gmail.com",
+      major: 'UI | Maquetadora',
+    },
+  ],
+  'FRIEND4': [{
+      name: 'Graciela Romo',
+      image: '../assets/images/friend4.jpg',
+      wichGeneration: '4° Generación | Teacher Jr.',
+      adress: 'Santiago de Chile',
+      mail: "grace-rm@hotmail.com",
+      major: 'UX research',
+    },
+  ],
+  'FRIEND5': [{
+    name: 'Xóchitl Campos',
+    image: '../assets/images/friend5.jpg',
+    wichGeneration: '5° Generación <l> | Current Student',
+    adress: 'CDMX',
+    mail: "xoch_camp@gmail.com",
+    major: 'Front-end Developer',
+  }, ]
+};
