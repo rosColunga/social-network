aa63c3bbf30201d38c4d69aabda2a8c8cf0bc213
rosColunga <rosicolunga@hotmail.com>
2018-01-30 12:11:34 -0600

    Añade funcionalidades regresar y accesar a home

--- js/log-in.js
+++ js/log-in.js
@@ -1,4 +1,19 @@
+$(document).ready(function() {
 
+  /*Se guarda clase botón "next" en variable*/
+  var nextButton = $('.nextJs');
+  nextButton.on("click", function(event) {
+    window.location.href = 'home.html';
+  });
+
+  /* Botón para regresar */
+  var goBack = $('.go-back');
+  goBack.on('click', function() {
+    /*Direcciona a vista previa "sign" al evento click*/
+    window.location.href = '../index.html';
+  });
+
+});
 
 // Initialize Firebase
 var config = {
