2d584087ae41aee1542325d1b031aa29b5ecacb0
rosColunga <rosicolunga@hotmail.com>
2018-01-30 11:09:37 -0600

    Comentamos console.log de Firebase

--- js/log-in.js
+++ js/log-in.js
@@ -32,18 +32,18 @@ function authentication(provider) {
     // ...
   })
   .catch(function(error) {
-    console.log(error);
+
     // Handle Errors here.
     var errorCode = error.code;
-    console.log(errorCode);
+    //console.log(errorCode);
     var errorMessage = error.message;
-    console.log(errorMessage);
+    //console.log(errorMessage);
     // The email of the user's account used.
     var email = error.email;
-    console.log(email);
+    //console.log(email);
     // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
-    console.log(credential);
+    //console.log(credential);
     // ...
   });
 }
